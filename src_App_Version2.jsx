853acd59ca4cd129cbfd8e118b4e0954445c89ab
MUCHALA <208081443+xiaoli2428@users.noreply.github.com>
2025-11-25 11:46:56 -0800

    Add files via upload
    Signed-off-by: MUCHALA <208081443+xiaoli2428@users.noreply.github.com>

--- src_App_Version2.jsx
+++ src_App_Version2.jsx
@@ -1,11 +1,18 @@
-// Example usage: src/App.jsx
-import React from 'react';
-import MultiTimezoneClock from './components/MultiTimezoneClock';
+import React from 'react'
+import Header from './components/Header'
+import Hero from './components/Hero'
+import Features from './components/Features'
+import Footer from './components/Footer'
 
 export default function App() {
   return (
-    <div style={{ padding: 18 }}>
-      <MultiTimezoneClock initialZones={['UTC', 'America/New_York', 'Europe/Berlin']} />
+    <div className="min-h-screen flex flex-col">
+      <Header />
+      <main className="flex-1">
+        <Hero />
+        <Features />
+      </main>
+      <Footer />
     </div>
-  );
+  )
 }
